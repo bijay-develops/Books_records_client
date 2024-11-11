94cf26cd7368f1d2aac904445db3a916b460f2c5
Bijay B.K. <bijaybk307@gmail.com>
2024-11-12 01:35:07 +0545

    UpdateBookInfo updated

--- src/components/UpdateBookInfo.jsx
+++ src/components/UpdateBookInfo.jsx
@@ -20,7 +20,7 @@ function UpdateBookInfo(props) {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8082/api/books/${id}`)
+      .get(`https://books-records.onrender.com/api/books/${id}`)
       .then((res) => {
         setBook({
           title: res.data.title,
@@ -55,7 +55,7 @@ function UpdateBookInfo(props) {
     };
 
     axios
-      .put(`http://localhost:8082/api/books/${id}`, data)
+      .put(`https://books-records.onrender.com/api/books/${id}`, data)
       .then((res) => {
         navigate(`/show-book/${id}`);
       })
